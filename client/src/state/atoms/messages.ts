5592d17ce7dd247ab1616db78d896f2e91bb31a4
tady <ohtukayoshi.yoshi@gmail.com>
2022-07-24 01:51:55 +0900

    remove unused atom

--- client/src/state/atoms/messages.ts
+++ client/src/state/atoms/messages.ts
@@ -1,11 +1,6 @@
 import { Message } from "./../../models/message";
 import { atom } from "recoil";
 
-export const messageAtom = atom<Message>({
-  key: "message",
-  default: undefined,
-});
-
 export const messageListAtom = atom<Message[]>({
   key: "messageList",
   default: [],
